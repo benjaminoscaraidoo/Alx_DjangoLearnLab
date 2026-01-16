2d91e9e2917e256f86f4dbe5bfc84b96633a922e
Benjamin Oscar Aidoo <benaidoojnr@gmail.com>
2026-01-16 23:45:08 +0000

    Author update for query

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,15 +1,13 @@
+from relationship_app.models import Author, Book, Library
 
-from relationship_app.models import Author, Book, Library, Librarian
 
 def query_books_by_author(author_name):
     """
     Query all books by a specific author
     """
-    books = Book.objects.filter(author__name=author_name)
-
-    print(f"Books by {author_name}:")
-    for book in books:
-        print(f"- {book.title}")
+    author = Author.objects.get(name=author_name)
+    books = Book.objects.filter(author=author)
+    return books
 
 
 def list_all_books_in_library(library_name):
@@ -17,13 +15,8 @@ def list_all_books_in_library(library_name):
     List all books in a library
     """
     library = Library.objects.get(name=library_name)
-
-    # works for ManyToMany or reverse ForeignKey
     books = library.books.all()
-
-    print(f"Books in library '{library_name}':")
-    for book in books:
-        print(f"- {book.title}")
+    return books
 
 
 def get_librarian_for_library(library_name):
@@ -31,14 +24,10 @@ def get_librarian_for_library(library_name):
     Retrieve the librarian for a library
     """
     library = Library.objects.get(name=library_name)
-    librarian = library.librarian
-
-    print(f"Librarian for '{library_name}':")
-    print(f"- {librarian.name}")
+    return library.librarian
 
 
 if __name__ == "__main__":
-    # sample calls
-    query_books_by_author("George Orwell")
-    list_all_books_in_library("Central Library")
-    get_librarian_for_library("Central Library")
+    print(query_books_by_author("George Orwell"))
+    print(list_all_books_in_library("Central Library"))
+    print(get_librarian_for_library("Central Library"))
