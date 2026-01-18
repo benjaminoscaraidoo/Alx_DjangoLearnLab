c8c82d1a0acf2bf68a34d339a94035cc61dc5da7
Benjamin Oscar Aidoo <benaidoojnr@gmail.com>
2026-01-18 15:36:02 +0300

    Adding views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from relationship_app.models import Book
+from django.views.generic import DetailView
+from relationship_app.models import Book, Library
 
 
 # Create your views here.
@@ -9,10 +10,17 @@ def list_books(request):
     Function-based view that lists all books with their authors
     """
     books = Book.objects.all()
-
-    #output = []
-    #for book in books:
-    #    output.append(f"{book.title} by {book.author}")
-
     context = {'book_list': books} 
     return render(request, 'relationship_app/list_books.html', context)
+
+
+class BookDetailView(DetailView):
+  """A class-based view for displaying details of a specific book."""
+  model = Book
+  template_name = 'relationship_app/library_detail.html'
+
+  def get_context_data(self, **kwargs):
+    """Injects additional context data specific to the book."""
+    context = super().get_context_data(**kwargs)  # Get default context data
+    book = self.get_object()  # Retrieve the current book instance
+    context['average_rating'] = book.get_average_rating() 
