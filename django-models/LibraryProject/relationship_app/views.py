2aa661c7586f9b7755cf4af2680d82404dbfbf60
Benjamin Oscar Aidoo <benaidoojnr@gmail.com>
2026-01-18 15:38:57 +0300

    Adding views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.generic import DetailView
+from django.views.generic.detail import DetailView
 from relationship_app.models import Book, Library
 from .models import Library
 
