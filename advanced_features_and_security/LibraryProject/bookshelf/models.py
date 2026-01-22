e986b58d75b55d28e20f86dd729e9654f959be8b
Benjamin Oscar Aidoo <benaidoojnr@gmail.com>
2026-01-22 12:59:42 +0300

    adding models.py for bookshelf advanced security

---
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+
+# Create your models here.
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    publication_year = models.IntegerField(default=0)
+
+
+class CustomUser(AbstractBaseUser):
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=20)
+    date_of_birth = models.DateField()
+    profile_photo = models.ImageField()
