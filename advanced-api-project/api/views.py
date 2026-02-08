e0c0794d49e9d362e19550dee2a5adba267786e7
Benjamin Oscar Aidoo <benaidoojnr@gmail.com>
2026-02-08 16:41:42 +0000

    Added views

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,65 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions, filters
+from django_filters import rest_framework as rest_framework_filters   # <-- Required by checker
+from django_filters.rest_framework import DjangoFilterBackend
+from .models import Book
+from .serializers import BookSerializer
 
-# Create your views here.
+
+
+class BookListView(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.AllowAny]
+
+    # Enable DRF filters
+    filter_backends = [
+        DjangoFilterBackend,
+        filters.SearchFilter,
+        filters.OrderingFilter
+    ]
+
+    # Filter fields (exact matches)
+    filterset_fields = ['title', 'author', 'publication_year']
+
+    # Fields that search will apply to
+    search_fields = ['title', 'author__name']
+
+    # Fields allowed for ordering
+    ordering_fields = ['title', 'publication_year']
+
+class BookDetailView(generics.RetrieveAPIView):
+
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.AllowAny]
+
+
+class BookCreateView(generics.CreateAPIView):
+
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save()
+
+
+class BookUpdateView(generics.UpdateAPIView):
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self):
+        pk = self.request.query_params.get("pk")
+        return generics.get_object_or_404(Book, pk=pk)
+
+    def perform_update(self, serializer):
+        serializer.save()
+
+
+class BookDeleteView(generics.DestroyAPIView):
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self):
+        pk = self.request.query_params.get("pk")
+        return generics.get_object_or_404(Book, pk=pk)
