12e45ef58eecbc50820cce11c6b29c101dc7a891
Benjamin Oscar Aidoo <benaidoojnr@gmail.com>
2026-02-15 20:55:08 +0000

    Updated setting with database details

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -80,6 +80,17 @@ DATABASES = {
     }
 }
 
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "blogdb",
+        "USER": "postgres",
+        "PASSWORD": "",
+        "HOST": "localhost",
+        "PORT": "5432",
+    }
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
